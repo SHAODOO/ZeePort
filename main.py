45b9af1b38157f782f3aacdc5b662afda2751d8a
McLiong) <manchun.liong178@gmail.com>
2023-01-14 03:43:29 +0800

    Live Host Module Upload

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import socket, sys, threading, ctypes, time
+import socket, sys, threading, ctypes, time, os
 from tkinter import *
 from getmac import get_mac_address
 from time import ctime
+from scapy.all import ARP, Ether, srp
 
 #importing library for GUI
 from tkinter import font
@@ -16,6 +17,76 @@ ip_f = 1024
 log = []
 ports = []
 target = 'localhost'
+
+# ==== Check Wifi's SSID and IP address ====
+def getCurrentWifiInfo():
+    global wifi_ssid, wifi_ip_address , mycomputer_hostname
+
+
+    ssid_search = os.popen("netsh wlan show interfaces").read()
+    search_result = ssid_search.split("\n")
+    SSID = list()
+    for i in search_result:
+        if "disconnected" in i:
+            wifi_ssid = "You are not connected to any internet connection"
+        elif "connected" in i:
+            for j in search_result:
+                if "BSSID" in j:
+                    pass
+                elif "SSID" in j:
+                    SSID.append(j[j.index(":")+2:])
+                    wifi_ssid = SSID[0]
+
+    mycomputer_hostname = socket.gethostname()
+    wifi_ip_address = socket.gethostbyname(mycomputer_hostname)
+    if wifi_ip_address == "127.0.0.1":
+	    wifi_ip_address = "-"
+
+# ==== Scan Live Host ====
+def ScanLiveHost():
+
+    livehost_list.delete(0,END)
+
+    target_ip=wifi_ip_address+"/24"
+    arp = ARP(pdst=target_ip)
+    ether =Ether(dst="ff:ff:ff:ff:ff:ff")
+    packet = ether/arp
+    livehost_result = srp(packet,timeout=3,verbose=0)[0]
+
+    livehost_list.insert(0, "Live Host's IP(s) :")
+
+    if wifi_ip_address != "-" :
+        for sent, received in livehost_result:
+            livehost_list.insert(1,received.psrc)
+            H4.configure(text="The Live Host Scanning Result for "+wifi_ssid+" Connection : ")
+    else:
+        livehost_list.insert(2,"No live host found.")
+        H4.configure(text="The Live Host Scanning Result Without Connection : ")
+
+        
+# ====LiveHostRefresh====
+def LHRefresh():
+    getCurrentWifiInfo()
+    H2.configure(text="Current Connected Internet     : "+wifi_ssid)
+    H3.configure(text="My IP Address in the Internet  : "+wifi_ip_address)
+    refresh_complete = Label(LH, text="Refresh Complete", font=("Cascadia Code", 12))
+    refresh_complete.place(x=16, y=210)
+    #refresh_complete.after(1000,refresh_complete.configure(text="Refresh Complete."))
+    #refresh_complete.after(1000,refresh_complete.configure(text="Refresh Complete.."))
+    #refresh_complete.after(3000,refresh_complete.configure(text="Refresh Complete..."))
+    refresh_complete.after(1000,refresh_complete.destroy)
+
+# ====Next Module to bring the IP to further action====
+def LHNext():
+    select_ip = livehost_list.get(livehost_list.curselection())
+    SP.pack(fill='both',expand=1)
+    LH.pack_forget()
+    L22.delete(0,END)
+    L22.insert(0, select_ip)
+
+def back():
+    LH.pack(fill='both',expand=1)
+    SP.pack_forget()
 
 def getMacAddress(ip_address):
     mac_address = get_mac_address(ip=ip_address)
@@ -237,64 +308,96 @@ w.destroy()
 w.mainloop()
 
 
-# ==== GUI ====
-gui = Tk()
-# set window title
-gui.title('ZeePort')
-# set window icon
-gui.iconbitmap('Logo.ico')
-# gui.geometry("500x700+40+40")
-width_of_window = 500
-height_of_window = 700
-screen_width = gui.winfo_screenwidth()
-screen_height = gui.winfo_screenheight()
-x_coordinate = (screen_width/2)-(width_of_window/2)
-y_coordinate = (screen_height/2)-(height_of_window/2)
-gui.geometry("%dx%d+%d+%d" %(width_of_window,height_of_window,x_coordinate,y_coordinate))
-
 # ==== Colors ====
 m1c = '#FFF4EA'         #text
 bgc = '#607D8B'         #background
 fgc = '#455A64'         #button clicked
 
-gui.tk_setPalette(background=bgc, foreground=m1c, activeBackground=fgc, activeForeground=bgc, highlightColor=m1c,
+# ==== LH ====
+win = Tk()
+win.title('ZeePort')
+# set window icon
+win.iconbitmap('Logo.ico')
+win.geometry("750x700+20+20")
+win.tk_setPalette(background=bgc, foreground=m1c, activeBackground=fgc, activeForeground=bgc, highlightColor=m1c,
                   highlightBackground=m1c)
 
+LH = Frame(win)
+getCurrentWifiInfo()
+LH.pack(fill='both',expand=1)
+H1 = Label(LH, text="ZeePort", font=("Algerian", 20)) #'underline'
+H1.place(x=16, y=10)
+H11 = Label(LH, text="Device's Host Name             : "+mycomputer_hostname, font=("Cascadia Code", 12))
+H11.place(x=16, y=80)
+H2 = Label(LH, text="Current Connected Internet     : "+wifi_ssid, font=("Cascadia Code", 12))
+H2.place(x=16, y=110)
+H3 = Label(LH, text="My IP Address in the Internet  : "+wifi_ip_address, font=("Cascadia Code", 12))
+H3.place(x=16, y=140)
+
+
+HB1 = Button(LH, text="Scan For Live Host", command=ScanLiveHost)
+HB1.place(x=550, y=170, width=170)
+HB2 = Button(LH, text="Refresh", command=LHRefresh)
+HB2.place(x=550, y=210, width=170)
+HB3 = Button(LH, text="Next", command=LHNext)
+HB3.place(x=550, y=600, width=170)
+
+
+ip_var=StringVar()
+
+frame = Frame(LH)
+frame.place(x=16, y=275, width=700, height=270)
+livehost_list = Listbox(frame, listvariable=ip_var, width=700, height=16,font=12,selectmode=SINGLE)
+livehost_list.place(x=0, y=0)
+livehost_list.bind('<<ListboxSelect>>')
+scrollbar = Scrollbar(frame)
+scrollbar.pack(side=RIGHT, fill=Y)
+livehost_list.config(yscrollcommand=scrollbar.set)
+scrollbar.config(command=livehost_list.yview)
+
+H4 = Label(LH, text="", font=("Cascadia Code", 12))
+H4.place(x=16, y=240)
+
+#win.mainloop()
+
+# ==== GUI ====
+SP = Frame(win)
+
+
 # ==== Labels ====
-L11 = Label(gui, text="ZeePort", font=("Algerian", 20)) #'underline'
+L11 = Label(SP, text="ZeePort", font=("Algerian", 20)) #'underline'
 L11.place(x=16, y=10)
 
-L21 = Label(gui, text="Target: ", font=("Cascadia Code", 12))
+L21 = Label(SP, text="Target: ", font=("Cascadia Code", 12))
 L21.place(x=16, y=90)
 
-L22 = Entry(gui, text="localhost", font=("Cascadia Code", 12))
+L22 = Entry(SP, text="localhost", font=("Cascadia Code", 12))
 L22.place(x=180, y=95)
-L22.insert(0, "localhost")
 
-L23 = Label(gui, text="Ports: ", font=("Cascadia Code", 12))
+L23 = Label(SP, text="Ports: ", font=("Cascadia Code", 12))
 L23.place(x=16, y=158)
 
-L24 = Entry(gui, text="1", font=("Cascadia Code", 12))
+L24 = Entry(SP, text="1", font=("Cascadia Code", 12))
 L24.place(x=180, y=163, width=95)
 L24.insert(0, "1")
 
-L25 = Entry(gui, text="1024", font=("Cascadia Code", 12))
+L25 = Entry(SP, text="1024", font=("Cascadia Code", 12))
 L25.place(x=290, y=163, width=95)
 L25.insert(0, "1024")
 
-L26 = Label(gui, text="Results: ", font=("Cascadia Code", 12))
+L26 = Label(SP, text="Results: ", font=("Cascadia Code", 12))
 L26.place(x=16, y=220)
-L27 = Label(gui, text="[ ... ]", font=("Cascadia Code", 12))
+L27 = Label(SP, text="[ ... ]", font=("Cascadia Code", 12))
 L27.place(x=180, y=222)
 
-L28 = Label(gui, text="", font=("Cascadia Code", 12))
+L28 = Label(SP, text="", font=("Cascadia Code", 12))
 L28.place(x=16, y=465)
 
-L29 =Label(gui, text="", font=("Cascadia Code", 12))
+L29 =Label(SP, text="", font=("Cascadia Code", 12))
 L29.place(x=16, y=490)
 
 # ==== Ports list ====
-frame = Frame(gui)
+frame = Frame(SP)
 frame.place(x=16, y=275, width=450, height=180)
 listbox = Listbox(frame, width=450, height=11)
 listbox.place(x=0, y=0)
@@ -305,10 +408,11 @@ listbox.config(yscrollcommand=scrollbar.set)
 scrollbar.config(command=listbox.yview)
 
 # ==== Buttons / Scans ====
-B11 = Button(gui, text="Start Scan", font=("Cascadia Code", 12), command=startScan)
+B11 = Button(SP, text="Start Scan", command=startScan)
 B11.place(x=16, y=540, width=170)
-B21 = Button(gui, text="Save Result", font=("Cascadia Code", 12), command=saveScan)
+B21 = Button(SP, text="Save Result", command=saveScan)
 B21.place(x=210, y=540, width=170)
-
+BB = Button(SP, text="Back", command=back)
+BB.place(x=16, y=600, width=170)
 # ==== Start GUI ====
-gui.mainloop()
+win.mainloop()
